454d0ef0125f146c9902d9e6a1e4aec643822cc2
Ursula Babeto <ursulacobabeto@gmail.com>
2023-10-12 20:47:14 -0300

    adiciona input

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,6 +1,9 @@
+import InputComponent from "../components/Input/Input.component";
 
 export default function Layout() {
   return (
-    <div>Layout is render</div>
+    <div>Layout is render 
+      <InputComponent type="text" id="ss" mask="999.999.999-99" label="cpf"></InputComponent>
+    </div>
   )
 }
