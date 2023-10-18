7bf06cb046296a39b3f55a3817e83ad176814b24
Ursula Babeto <ursulacobabeto@gmail.com>
2023-10-17 22:28:52 -0300

    adiciona pagina 404

---
+++ src/pages/NotFound/NotFound.page.jsx
@@ -0,0 +1,14 @@
+export default function NotFoundPage() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: "100px",
+      }}
+    >
+      <img src="./src/assets/images/not_found.jpg" width={600} />
+    </div>
+  );
+}
