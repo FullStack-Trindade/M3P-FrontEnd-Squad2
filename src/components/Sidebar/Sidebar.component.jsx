02b31e6b4092ec7caa420aa0863cae53757b7230
Rodolfo <6982920+rpad88@users.noreply.github.com>
2023-10-18 21:23:44 -0400

    feito o sidebarContext e melhorada a estrutura do Sidebar

--- src/components/Sidebar/Sidebar.component.jsx
+++ src/components/Sidebar/Sidebar.component.jsx
@@ -1,22 +1,46 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
 import * as Styled from "./sidebar.style"
 import {
 	TbLayoutSidebarLeftCollapse,
 	TbLayoutSidebarLeftExpand,
 	TbLogout,
+	TbUserCircle,
+	TbHome,
 } from "react-icons/tb"
 
+import { FaUserDoctor, FaUserNurse } from 'react-icons/fa6'
+import { MdOutlineSick } from 'react-icons/md'
+import { useNavigate } from "react-router-dom"
+import { SidebarContext } from "../../contexts/SidebarContext"
+
 export default function SidebarComponent() {
-	const [isOpen, setIsOpen] = useState(true)
+
+	// CONTEXTS
+	const { showSidebar, setShowSidebar} = useContext(SidebarContext)
+
+	// REACT-ROUTER-DOM
+	const navigate = useNavigate()
+
+	// FUNCTIONS
+	const logout = async () => {
+		// deslogar e apagar token no localStorage
+
+		// após logout redirecionar para a página de login
+		// navigate('/login')
+	}
+
+	const check = (e) => {
+		console.log(e.target.id)
+	}
 
 	return (
-		<Styled.Sidebar isOpen>
+		<Styled.Sidebar show={showSidebar} $isOpened={showSidebar}>
 			<Styled.Header>
 				<div
 					style={{ width: "100%", display: "flex", justifyContent: "end" }}
 				>
-					<button onClick={() => setIsOpen(!isOpen)}>
-						{isOpen ? (
+					<button onClick={() => setShowSidebar(!showSidebar)}>
+						{showSidebar ? (
 							<TbLayoutSidebarLeftCollapse size={"1.5rem"} />
 						) : (
 							<TbLayoutSidebarLeftExpand size={"1.5rem"} />
@@ -27,26 +51,41 @@ export default function SidebarComponent() {
 			</Styled.Header>
 			<Styled.Body>
 				<Styled.ListGroup>
+					<p style={{display: showSidebar ? "" : "none"}}>Páginas</p>
+
 					<ul>
                         <li>
                             <input type="radio" name="selector" id="home" />
-                            <label htmlFor="home">Home</label>
+							<TbHome size={showSidebar ? "" : "1.5rem"} />
+                            <label htmlFor="home" style={{display: showSidebar ? "" : "none"}}>Home</label>
+                        </li>
+                        <li>
+                            <input type="radio" name="selector" id="cadMedico" />
+							<FaUserDoctor size={showSidebar ? "" : "1.5rem"} />
+                            <label htmlFor="cadMedico" style={{display: showSidebar ? "" : "none"}}>Cad. Medico</label>
+                        </li>
+						<li>
+                            <input type="radio" name="selector" id="cadEnfermeiro" />
+							<FaUserNurse size={showSidebar ? "" : "1.5rem"} />
+                            <label htmlFor="cadEnfermeiro" style={{display: showSidebar ? "" : "none"}}>Cad. Enfermeiro</label>
                         </li>
                         <li>
                             <input type="radio" name="selector" id="cadPaciente" />
-                            <label htmlFor="cadPaciente">Cad. Paciente</label>
-                        </li>
-                        <li>
-                            <input type="radio" name="selector" id="cadEnfermeiro" />
-                            <label htmlFor="cadEnfermeiro">Cad. Enfermeiro</label>
-                        </li>
+							<MdOutlineSick size={showSidebar ? "" : "1.5rem"} />
+                            <label htmlFor="cadPaciente" style={{display: showSidebar ? "" : "none"}}>Cad. Paciente</label>
+                        </li>                        
                     </ul>
 				</Styled.ListGroup>
 			</Styled.Body>
 			<Styled.Footer>
-				<button>
+				<Styled.UserContainer $isOpened={showSidebar}>
+					<TbUserCircle size={"1.5rem"}/>
+					<span>Username</span>
+				</Styled.UserContainer>
+
+				<Styled.LogoutBtn onClick={logout} $isOpened={showSidebar}>
 					<TbLogout size={"1.5rem"} />
-				</button>
+				</Styled.LogoutBtn>
 			</Styled.Footer>
 		</Styled.Sidebar>
 	)
--- src/components/Sidebar/sidebar.style.jsx
+++ src/components/Sidebar/sidebar.style.jsx
@@ -5,12 +5,11 @@ export const Sidebar = styled.nav`
 	flex-direction: column;
 	background-color: #004a63;
 	color: #fefefe;
-	width: 250px;
 	height: 100vh;
 	min-height: 100%;
-	/* width: ${({ $isOpened }) => {
-		return $isOpened ? "280px" : "70px"
-	}}; */
+	width: ${({ $isOpened }) => {
+		return $isOpened ? "200px" : "60px"
+	}};
 	inset: 0;
 	padding: 0.5rem;
 `
@@ -36,39 +35,85 @@ export const Header = styled.div`
 
 export const Body = styled.div`
 	width: 100%;
-    height: 100%;
+	height: 100%;
 	display: flex;
 	flex-direction: column;
 	color: #888888;
-    margin: 1rem 0;
+	margin: 1rem 0;
 `
 export const ListGroup = styled.div`
+	margin: 1rem 0;
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
 
-    li::marker {
-        content: "";
-    }
+	p {
+		display: flex;
+		justify-content: center;
+		color: #fefefe;
+	}
 
-    input[type="radio"] {
-        display: none;
-    }
+	li {
+		padding: 10px;
+		display: flex;
+		align-content: center;
+		align-items: baseline;
+		border-radius: .5rem;
+		gap: 5px;;
+	}
 
-    input[type="radio"]:checked + label{
-        color: limegreen;
-    }
+	li:hover {
+		cursor: pointer;
+		background-color: #888;
+		
+		& > * {
+			color: #fff;
+		}
+	}
+
+	li::marker {
+		content: "";
+	}
+
+	li label:hover {
+		cursor: pointer;
+	}
+
+	input[type="radio"] {
+		display: none;
+	}
+
+	input[type="radio"]:checked + label {
+		color: limegreen;
+	}
 `
 
 export const Footer = styled.div`
 	width: 100%;
 	display: flex;
-	justify-content: flex-end;
-	button {
-		padding: 0;
-		background-color: transparent;
-		border: 0;
-		color: #fefefe;
+	flex-direction: column;
+	/* justify-content: flex-end; */
+`
 
-		&:hover {
-			cursor: pointer;
-		}
+export const UserContainer = styled.div`
+	display: flex;
+	flex-direction: ${({ $isOpened }) => {
+		return $isOpened ? "row" : "column"
+	}};
+	gap: 0.5rem;
+	padding: .5rem;
+`
+
+export const LogoutBtn = styled.button`
+	align-self: ${({ $isOpened }) => {
+		return $isOpened ? "flex-end" : "center"
+	}};
+	padding: 0;
+	background-color: transparent;
+	border: 0;
+	color: #fefefe;
+
+	&:hover {
+		cursor: pointer;
 	}
 `
