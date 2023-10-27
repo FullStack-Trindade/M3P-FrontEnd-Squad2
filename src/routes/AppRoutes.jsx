f8d98be0d7aa81bb68713411be9e54820e8aa386
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-10-27 20:13:26 -0300

    atualizado route para testar mockpaciente

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "../layouts/Layout";
 import NotFoundPage from "../pages/NotFound/NotFound.page";
+import MockPaciente from "../pages/CadastroPaciente/mockPaciente";
 
 
 export const AppRoutes = () => {
@@ -8,6 +9,7 @@ export const AppRoutes = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}/>
+        <Route path="/mockpaciente" element={<MockPaciente />} />
         <Route path="*" element={<NotFoundPage />}/>       
       </Routes>
     </Router>
