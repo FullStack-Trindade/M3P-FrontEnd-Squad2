01bb405d9788685422afa79cd8ca57a5d9d964e0
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-10-27 06:52:33 -0300

    ajuste de layout pos merge side tool e components

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -2,33 +2,35 @@ import { Btn } from "../components/Button/button.style";
 import { ToolbarComponent } from "../components/Toolbar/Toolbar.component";
 import { ToolbarTituloProvider } from "../contexts/ToolbarTitulo/ToolbarTitulo.context";
 import InputComponent from "../components/Input/Input.component";
-import SidebarComponent from "../components/Sidebar/Sidebar.component"
+import SidebarComponent from "../components/Sidebar/Sidebar.component";
 
 export default function Layout() {
-	return (
-		<ToolbarTituloProvider>
-    <div style={{display: 'flex'}}>
-			<SidebarComponent />
-			<div>
-				Layout is render
-				<InputComponent type="text" id="ss" mask="999.999.999-99" label="cpf"></InputComponent>
-				<div>
-					<ToolbarComponent />
-					<h4>Botões e suas variantes</h4>
-					<div style={{ display: "flex", gap: "1rem" }}>
-						<Btn variant="primary">primary</Btn>
-						<Btn variant="outlined">outlined</Btn>
-						<Btn variant="info">info</Btn>
-						<Btn variant="red">red</Btn>
-						<Btn variant="redOutlined">redOutlined</Btn>
-						<Btn variant="green">green</Btn>
-						<Btn variant="greenOutlined">greenOutlined</Btn>
-						<Btn variant="blue">blue</Btn>
-						<Btn variant="blueOutlined">blueOutlined</Btn>
-					</div>
-				</div>
-			</div>
-    </div>
-		</ToolbarTituloProvider>
-	);
+  return (
+    <ToolbarTituloProvider>
+      <div style={{ display: "flex" }}>
+        <SidebarComponent />
+          <div>
+            <ToolbarComponent />
+            <h4>Botões e suas variantes</h4>
+            <div style={{ display: "flex", gap: "1rem" }}>
+              <Btn variant="primary">primary</Btn>
+              <Btn variant="outlined">outlined</Btn>
+              <Btn variant="info">info</Btn>
+              <Btn variant="red">red</Btn>
+              <Btn variant="redOutlined">redOutlined</Btn>
+              <Btn variant="green">green</Btn>
+              <Btn variant="greenOutlined">greenOutlined</Btn>
+              <Btn variant="blue">blue</Btn>
+              <Btn variant="blueOutlined">blueOutlined</Btn>
+            </div>
+			<InputComponent
+            type="text"
+            id="ss"
+            mask="999.999.999-99"
+            label="cpf"
+          ></InputComponent>
+          </div>
+        </div>
+    </ToolbarTituloProvider>
+  );
 }
