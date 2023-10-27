50830126b2926ae66ed113430df8ede4f39f1544
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-10-27 06:24:10 -0300

    Revert "Components/sidebar"

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,27 +1,6 @@
-import { Btn } from "../components/Button/button.style"
-import SidebarComponent from "../components/Sidebar/Sidebar.component"
 
 export default function Layout() {
-	return (
-		<div style={{display: 'flex'}}>
-			<SidebarComponent />
-			<div>
-				Layout is render
-				<div>
-					<h4>Botões e suas variantes</h4>
-					<div style={{ display: "flex", gap: "1rem" }}>
-						<Btn variant="primary">primary</Btn>
-						<Btn variant="outlined">outlined</Btn>
-						<Btn variant="info">info</Btn>
-						<Btn variant="red">red</Btn>
-						<Btn variant="redOutlined">redOutlined</Btn>
-						<Btn variant="green">green</Btn>
-						<Btn variant="greenOutlined">greenOutlined</Btn>
-						<Btn variant="blue">blue</Btn>
-						<Btn variant="blueOutlined">blueOutlined</Btn>
-					</div>
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div>Layout is render</div>
+  )
 }
