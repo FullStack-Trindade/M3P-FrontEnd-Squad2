cd1d9701a6580977cc724846ea2c42974baffdd8
Rodolfo <6982920+rpad88@users.noreply.github.com>
2023-10-28 17:14:12 -0400

    adicionado o modal

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client"
 import { AppRoutes } from "./routes/AppRoutes"
 import { GlobalStyle } from "./global.style"
 import { SidebarProvider } from "./contexts/SidebarContext"
+import { ModalProvider } from "./contexts/Modal.context"
 //
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<GlobalStyle />
 		<SidebarProvider>
-			<AppRoutes />
+			<ModalProvider>
+				<AppRoutes />
+			</ModalProvider>
 		</SidebarProvider>
 	</React.StrictMode>
 )
