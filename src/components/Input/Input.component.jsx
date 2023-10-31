b290cc81b09f169756e71a4e4f1df607600ddc8f
Ursula Babeto <ursulacobabeto@gmail.com>
2023-10-31 16:28:57 -0300

    component com exemplo do inputMask

--- src/components/Input/Input.component.jsx
+++ src/components/Input/Input.component.jsx
@@ -1,47 +1,114 @@
-import PropTypes from 'prop-types';
-import { useState } from 'react';
-import { MdVisibility, MdVisibilityOff } from 'react-icons/md';
+import { useState } from "react";
+import { MdVisibility, MdVisibilityOff } from "react-icons/md";
+import * as Styled from "./Input.style";
+import { COLOR } from "../../assets/styles.colors";
+import PropTypes from "prop-types";
+import InputMask from "react-input-mask"
 
-import * as Styled from './Input.style';
-
-export default function InputComponent ({mask, label, type, id, placeholder, register, error }) {
+const InputComponent = ({
+  label,
+  type,
+  id,
+  placeholder,
+  onBlur,
+  isLoading,
+  register,
+  error,
+  value,
+  rows,
+  defaultValue,
+}) => {
   const [showPassword, setShowPassword] = useState(false);
 
   const handleShowPassword = () => {
     setShowPassword(!showPassword);
-  }
+  };
 
-  return(
-   <> <Styled.InputGroup>
-   <Styled.Label $color={error && 'danger'} htmlFor={ id } >{ label }</Styled.Label>
+  return (
+    <Styled.InputGroup>
+      <Styled.Label $color={error && "danger"} htmlFor={id}>
+        {label}
+      </Styled.Label>
+      {type !== "textarea" && (
+        <Styled.InputContainer>
+          <Styled.Input
+            $color={error && "danger"}
+            type={showPassword ? "text" : type}
+            id={id}
+            placeholder={placeholder}
+            onBlur={onBlur}
+            {...register}
+            value={value || defaultValue}
+          />
+          <Styled.Loading>
+            {isLoading && (
+              <ReactLoading
+                type="spin"
+                style={{ color: COLOR.$blue_dark }}
+                width={"2rem"}
+              />
+            )}
+          </Styled.Loading>
+          {type === "password" && (
+            <Styled.Icon
+              $color={error && "danger"}
+              type="button"
+              onClick={handleShowPassword}
+            >
+              {!showPassword ? <MdVisibility /> : <MdVisibilityOff />}
+            </Styled.Icon>
+          )}
+          {error && <Styled.Error>{error.message}</Styled.Error>}
+        </Styled.InputContainer>
+      )}
 
-   { type !== 'textarea' &&
-     <Styled.InputContainer>
-       <Styled.Input $color={error && 'danger'} type={ showPassword ? 'text' : type } id={ id } placeholder={ placeholder } {...register} mask={mask}/>
-       { type === 'password' && 
-         <Styled.Icon $color={error && 'danger'} type='button' onClick={handleShowPassword}>
-           { !showPassword 
-               ? <MdVisibility/>
-               : <MdVisibilityOff/>
-           }
-         </Styled.Icon>
-       }
-     </Styled.InputContainer>
-   }
+      {type === "textarea" && (
+        <Styled.TextAreaContainer>
+          <Styled.TextArea
+            $color={error && "danger"}
+            id={id}
+            placeholder={placeholder}
+            rows={rows}
+            value={value || defaultValue}
+            {...register}
+          />
+          {error && <Styled.Error>{error.message}</Styled.Error>}
+        </Styled.TextAreaContainer>
+      )}
+    </Styled.InputGroup>
+  );
+};
 
-   { type === 'textarea' &&
-     <Styled.TextArea $color={error && 'danger'} id={ id } placeholder={ placeholder } {...register}/>
-   }
- </Styled.InputGroup></>
-  )
-}
+export default InputComponent;
 
 InputComponent.propTypes = {
   label: PropTypes.string,
   type: PropTypes.string,
   id: PropTypes.string.isRequired,
   placeholder: PropTypes.string,
-  register: PropTypes.any,
   error: PropTypes.any,
+  register: PropTypes.any,
   mask: PropTypes.any,
-}
+};
+
+
+/*     
+;  
+
+<InputMask
+          name="cpf"
+          mask={"999.999.999-99"}      
+          maskChar=""
+        >
+          {(inputprops) => (
+            <InputComponent
+              {...inputprops}
+              label="CPF"
+              id="cpf"
+              type="text"
+              placeholder="123.123.123-12"
+              register={register("cpf")}          
+            
+            />            
+          )}
+        </InputMask> */
