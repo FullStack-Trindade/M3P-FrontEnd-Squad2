eab11f0efc14eb08a1a5c94af8ca6fc1840ed950
André Luiz Amorim de Souza <andre_luiz_amorim@estudante.sc.senai.br>
2023-10-31 22:48:12 -0300

    Adicionando nome do usuario e logout na toolbar

--- src/components/Toolbar/Toolbar.component.jsx
+++ src/components/Toolbar/Toolbar.component.jsx
@@ -2,25 +2,32 @@ import { TbUserCircle, TbLogout } from "react-icons/tb";
 import * as Styled from "./Toolbar.style";
 import { useToolbarContext } from "../../hooks/useToolbarContext";
 import { useState } from "react";
+import { useAuth } from "../../hooks/useAuth";
 
 export const ToolbarComponent = () => {
 	const { titulo } = useToolbarContext();
 
 	const [abrirDropdown, setAbrirDropdown] = useState(false);
 
+	const { usuario, logout } = useAuth();
+
 	const handleAbrirDropdown = () => {
 		setAbrirDropdown(!abrirDropdown);
 	};
+
+	const handleSair = async () => {
+		logout();
+	}
 
 	return (
 		<Styled.Header>
 			<Styled.TituloPagina>{titulo}</Styled.TituloPagina>
 			<Styled.Perfil onClick={handleAbrirDropdown}>
-				<Styled.NomePerfil>Nome Usuario</Styled.NomePerfil>
+				<Styled.NomePerfil>{usuario.nomeCompleto}</Styled.NomePerfil>
 				<TbUserCircle />
 				<Styled.PerfilDropdown $hasOpen={abrirDropdown}>
 					{/* opcional para adicionar link para configurações aqui <Styled.PerfilDropdownItem></Styled.PerfilDropdownItem> */}
-					<Styled.PerfilDropdownItem>
+					<Styled.PerfilDropdownItem onClick={handleSair}>
 						<TbLogout />
 						Sair
 					</Styled.PerfilDropdownItem>
