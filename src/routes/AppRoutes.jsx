71bfd7b6492ee47dbfc6890782b0bcd6d7612f1c
André Luiz Amorim de Souza <andre_luiz_amorim@estudante.sc.senai.br>
2023-10-31 22:49:33 -0300

    Criando rotas privadas para teste de autenticação

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,13 +2,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "../layouts/Layout";
 import NotFoundPage from "../pages/NotFound/NotFound.page";
 import { LoginPage } from "../pages/LoginPage/Login.page";
+import { PrivateRoutes } from "./PrivateRoutes";
+import { HomePage } from "../pages/Home/Home.page";
 
 
 export const AppRoutes = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path="/" element={<Layout />}>
+            <Route path="/home" element={<HomePage />} />
+          </Route>
+        </Route>
         <Route path='/login' element={<LoginPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
